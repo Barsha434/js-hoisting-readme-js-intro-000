5373e241ef09314583ed688c8938c9ef902d2e1e
Barsha Dahal <dahalbarsha19@gmail.com>
2020-05-30 02:05:50 +0000

    Automatically backed up by Learn

--- hoisting.js
+++ hoisting.js
@@ -18,16 +18,12 @@ function crazy(){
 
 
 
-
-
 function sayMyName() {
   // fix the code in here:
-  var name = "Cricky";
+  var name = "Kristin";
 
-  sayMy();
-
-  function sayMy() {
-    console.log(name);
-    var name = "Kristin";
+  var sayMy= function() {
+    console.log(name);  
   }
+  sayMy()
 }
